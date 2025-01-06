a562a8f8728530e345a6e6581dc19301669ddd25
SergioSantana07 <sergiosantanafilho07@gmail.com>
2025-01-06 19:38:14 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "qb7k7d",
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {
-    setupNodeEvents(on, config) {},
-  },
+  e2e: {},
 })
